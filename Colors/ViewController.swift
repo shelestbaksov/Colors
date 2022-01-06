a46475d46e735b1d2cbe28602a3d43e4e03985cd
Leisan Latypova <latley90@gmail.com>
2022-01-06 14:32:45 +0300

    Initial Commit

---
+++ Colors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Leysan Latypova on 06.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
