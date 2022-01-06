067c8b7fe24e5b4a747a9f207218ab3a3ad16106
Leisan Latypova <latley90@gmail.com>
2022-01-06 19:11:07 +0300

    Colors

--- Colors/ViewController.swift
+++ Colors/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
     }
 
-
+    func changeColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    @IBAction func rgbSliderChanged(_ sender: Any) {
+        redLabel.text = String(format:"%.1f", redSlider.value)
+        greenLabel.text = String(format:"%.1f", greenSlider.value)
+        blueLabel.text = String(format:"%.1f", blueSlider.value)
+        changeColor()
+    }
 }
-
+    
